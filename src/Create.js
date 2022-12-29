23818893f68120c61aaabc4111e1cb2b355f475a
Kyron Smith <kds060902@outlook.com>
2022-12-29 18:42:46 +0000

    added layout of create blog page

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,28 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title,setTitle] =useState("")
+    const [body,setBody]=useState("")
+    const [author, setAuthor]=useState("mario")
+
     return ( 
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form>
+                <label>Blog Title:</label>
+                <input type="text" required value={title} onChange={(e)=>setTitle(e.target.value)}/>
+            </form>
+            <form>
+                <label>Blog Body:</label>
+                <textarea required value={body} onChange ={(e)=>setBody(e.target.value)}>
+                </textarea>
+                <label>Blog Author:</label>
+                <select value={author} onChange={(e)=> setAuthor(e.target.value)}>
+                    <option value="mario">mario</option>
+                    <option value="luigi">luigi</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
      );
 }
