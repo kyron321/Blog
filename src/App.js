4b036045b8cc02b06700b0e422e3e3472030ea5e
Kyron Smith <kds060902@outlook.com>
2022-12-29 16:05:46 +0000

    setup basic components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./Home"
 
 function App() {
   return (
-    <div>
-      text
+    <div className="App">
+      <Navbar />
+      <div className="content">
+      <Home/>
+      </div>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>Kyrons Blog</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create">Home</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
