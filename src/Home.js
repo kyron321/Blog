71d703b05bda41a4392187669f3abad9ae839d8a
Kyron Smith <kds060902@outlook.com>
2022-12-29 17:29:18 +0000

    added conditonal rendering for loading message

--- src/Home.js
+++ src/Home.js
@@ -3,10 +3,7 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
-
-  const [name, setName] = useState("mario");
-
-
+  const [isPending, setIsPending] = useState(true)
 
   useEffect(() => {
     fetch("http://localhost:8000/blogs")
@@ -15,11 +12,13 @@ const Home = () => {
       })
       .then((data) => {
         setBlogs(data);
+        setIsPending(false)
       });
   }, []);
 
   return (
     <div className="home">
+        {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All the blogs!"/>}
     </div>
   );
