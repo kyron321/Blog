8116a5b7638e38bae1acc5a2146990ec5fc89e54
Kyron Smith <kds060902@outlook.com>
2022-12-29 18:24:11 +0000

    added detailed react routers for endpoints

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>Kyrons Blog</h1>
       <div className="links">
-        <a href="/">Home</a>
-        <a href="/create">Create Blog</a>
+        <Link to="/">Home</Link>
+        <Link to="/create">Create Blog</Link>
       </div>
     </nav>
   );
--- src/useFetch.js
+++ src/useFetch.js
@@ -1,13 +1,13 @@
 import { useState, useEffect } from "react";
 
 const useFetch = (url) => {
-
   const [data, setData] = useState(null);
   const [isPending, setIsPending] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch(url)
+    const abortCont = new AbortController();
+    fetch(url, { signal: abortCont.signal })
       .then((res) => {
         if (!res.ok) {
           throw Error("Could not fetch data");
@@ -20,11 +20,16 @@ const useFetch = (url) => {
         setError(null);
       })
       .catch((err) => {
-        setIsPending(false);
-        setError(err.message);
+        if (err.name === "AbortError") {
+          console.log("fetch aborted");
+        } else {
+          setIsPending(false);
+          setError(err.message);
+        }
       });
+    return () => abortCont.abort();
   }, [url]);
-  return {data, isPending, error}
+  return { data, isPending, error };
 };
 
 export default useFetch;
